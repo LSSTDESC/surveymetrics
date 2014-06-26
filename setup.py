33a4eb1a9d7136325caf8150643133ac8341b303
Alex Kim <agkim@lbl.gov>
2014-06-26 08:22:43 -0700

    First version of setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='surveymetrics',
+      version='0.0',
+      description='Metrics to Assess Astronomical Surveys',
+      author='Alex Kim',
+      author_email='agkim@lbl.gov',
+      packages=['surveymetrics', 'projects'],
+     )
