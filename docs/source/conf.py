0e313d8780ece4e34ff4fc8c5ff6e31094330fdb
Alex Kim <agkim@lbl.gov>
2014-08-07 11:40:04 -0700

    Try again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,11 +15,16 @@
 import sys
 import os
 
-import mock
-MOCK_MODULES = ['matplotlib.pyplot']
+from mock import Mock as MagicMock
 
-for mod_name in MOCK_MODULES:
-  sys.modules[mod_name] = mock.Mock()
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['matplotlib.pyplot']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
